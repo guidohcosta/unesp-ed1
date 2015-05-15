83c3a08d06e13bcf1c736363dfb81d4a9421438a
Guilherme Costa <guidohcosta@gmail.com>
2015-05-14 21:56:09 -0300

    Exercicios de lista

---
+++ 14-maio/exercicio-2.cpp
@@ -0,0 +1,10 @@
+/*
+ * Qual o primeiro no da lista?
+ * Qual o ultimo no da lista?
+ * Quantos nos tem a lista?
+ *
+ * Pensar sobre:
+ *  Insercao antes do primeiro no
+ *  Insercao em lista vazia
+ *  Eliminacao do primeiro no
+ */
